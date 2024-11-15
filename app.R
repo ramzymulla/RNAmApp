1f56a0c751b6a316ce1de236deea8e1613d64102
Ramzy Al-Mulla <rza@n0349.talapas.uoregon.edu>
2024-11-15 14:06:17 -0800

    20241115

--- app.R
+++ app.R
@@ -13,22 +13,22 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("RNA Mapper Shiny App!"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            fileInput("file1","Choose Aligned SAM or BAM file",
+            fileInput("Genome","Choose Aligned SAM or BAM file",
                       multiple=F,
                       accept = c("text/.sam",
                                  "text/.bam",
-                                 ".sam",".bam"
-                                 ))
+                                 ".sam",".bam")
+            ),
+            fileInput("SNPdb", "Choose SNP database",
+                        multiple=F,
+                        accept = c("text/.vcf", ".vcf")),
+            
+                            
         ),
 
         # Show a plot of the generated distribution
