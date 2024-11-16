3ff0bac513f6a614bfbacde3e98d5ef7663d16a4
ramzymulla <rza@uoregon.edu>
2024-11-15 22:42:16 -0800

    20241115

--- RNAmApp.R
+++ RNAmApp.R
@@ -1,11 +1,57 @@
 library(shiny)
 library(shinydashboard)
 
+raw_read_accepts = c("text/.fastq", "text/.fastq.gz",
+                     "text/.fasta", "text/fasta.gz",
+                     "text/.fa", "text/fa.gz",
+                      "text/.fq.gz","text/.fq", 
+                      ".fq", ".fq.gz", ".fastq", "fastq.gz",
+                     ".fasta",".fasta.gz","fa","fa.gz")
+
+genome_accepts = c("text/.sam", "text/.bam", ".sam", ".bam")
+
+variant_accepts = c("text/.vcf", ".vcf", "text/.vcf.gz",".vcf.gz")
+
 # Define UI using shinydashboard
 ui <- dashboardPage(
-  dashboardHeader(title = "RNA Mapper"),
+  dashboardHeader(
+    title = tags$div(
+      tags$img(src = "uo_logo_funny.png", height = "40px", style = "margin-right: 10px;"),
+      "RNA Mapper"
+    ),
+    titleWidth = 300
+  ),
   
   dashboardSidebar(
+    width = 300,
+    tags$head(tags$style(HTML("
+      .skin-blue .main-sidebar {
+        background-color: #154733;
+      }
+      .skin-blue .main-sidebar .sidebar-menu > li > a {
+        color: #FFFFFF;
+      }
+      .skin-blue .main-sidebar .sidebar-menu > li.active > a {
+        background-color: #FCE020;
+        color: #154733;
+      }
+      .skin-blue .main-header .navbar {
+        background-color: #154733;
+      }
+      .skin-blue .main-header .logo {
+        background-color: #154733;
+        color: #FCE020;
+        font-size: 18px;
+        font-weight: bold;
+      }
+      .skin-blue .main-header .logo:hover {
+        background-color: #FCE020;
+        color: #154733;
+      }
+      .content-wrapper, .right-side {
+        background-color: #FFFFFF;
+      }
+    "))),
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")),
       menuItem("Step 1: File Upload", tabName = "step_1", icon = icon("upload")),
@@ -17,7 +63,8 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       # Home Page
-      tabItem(tabName = "home", 
+      tabItem(tabName = "home",
+              tags$img(src = "MillerLogo1.jpg", height = "150px", style = "display: block; margin: auto;"),
               h3("Welcome to the RNA Mapper Pipeline!"),
               p("This app will guide you through the process of identifying causative mutations from bulk RNA-seq data."),
               p("Please use the menu on the left to navigate through different stages of the pipeline."),
@@ -41,25 +88,35 @@ ui <- dashboardPage(
               
               tabsetPanel(
                 id = "file_upload_tabs",
-                tabPanel("Configuration 1",
-                         h4("Configuration 1: Basic File Upload"),
-                         fileInput("wt_reads_1", "Upload wild type RNA-seq data (SAM or BAM format)", 
+                tabPanel("Raw Reads",
+                         h4("File Upload for Raw Reads"),
+                         fileInput("wt_reads_1", "Upload wild type RNA-seq data (FASTA or FASTQ format)", 
                                    multiple = FALSE,
-                                   accept = c("text/.sam", "text/.bam", ".sam", ".bam")),
-                         fileInput("mut_reads_1", "Upload mutant RNA-seq data (SAM or BAM format)", 
+                                   accept = raw_read_accepts),
+                         fileInput("mut_reads_1", "Upload mutant RNA-seq data (FASTA or FASTQ format)", 
                                    multiple = FALSE,
-                                   accept = c("text/.sam", "text/.bam", ".sam", ".bam")),
+                                   accept = raw_read_accepts),
+                         fileInput("SNPdb_1", "Upload VCF containing known mutations", 
+                                   multiple = FALSE,
+                                   accept = variant_accepts),
+                         textInput("MUTdb_url_1", "...or provide a URL download link"),
                          actionButton("upload_button_1", "Upload Files for Configuration 1"),
-                         uiOutput("upload_status_1")
+                         uiOutput("upload_status")
                 ),
-                tabPanel("Configuration 2",
-                         h4("Configuration 2: VCF File Upload"),
+                tabPanel("Aligned Reads",
+                         h4("File Upload for Aligned Reads (must be aligned using HISAT2!!"),
+                         fileInput("wt_reads_2", "Upload wild type RNA-seq data (SAM or BAM format)", 
+                                   multiple = FALSE,
+                                   accept = genome_accepts),
+                         fileInput("mut_reads_2", "Upload mutant RNA-seq data (SAM or BAM format)", 
+                                   multiple = FALSE,
+                                   accept = genome_accepts),
                          fileInput("SNPdb_2", "Upload VCF containing known mutations", 
                                    multiple = FALSE,
-                                   accept = c("text/.vcf", ".vcf")),
+                                   accept = variant_accepts),
                          textInput("MUTdb_url_2", "...or provide a URL download link"),
                          actionButton("upload_button_2", "Upload Files for Configuration 2"),
-                         uiOutput("upload_status_2")
+                         uiOutput("upload_status")
                 )
               )
       ),
@@ -84,31 +141,36 @@ ui <- dashboardPage(
 
 # Define server logic
 server <- function(input, output, session) {
+  # Unified reactive values to store inputs
+  unified_inputs <- reactiveValues(
+    wt_reads = NULL,
+    mut_reads = NULL,
+    SNPdb = NULL,
+    MUTdb_url = NULL
+  )
   
-  # Handle file upload for Configuration 1
+  # Handle file uploads for Configuration 1
   observeEvent(input$upload_button_1, {
-    if (is.null(input$wt_reads_1) || is.null(input$mut_reads_1)) {
-      output$upload_status_1 <- renderUI({
-        p("Please upload all required files for Configuration 1.")
-      })
-    } else {
-      output$upload_status_1 <- renderUI({
-        p("Files successfully uploaded for Configuration 1!")
-      })
-    }
+    unified_inputs$wt_reads <- input$wt_reads_1
+    unified_inputs$mut_reads <- input$mut_reads_1
+    unified_inputs$SNPdb <- NULL  # Not used in this configuration
+    unified_inputs$MUTdb_url <- NULL  # Not used in this configuration
+    
+    output$upload_status <- renderUI({
+      p("Files successfully uploaded for Configuration 1!")
+    })
   })
   
-  # Handle file upload for Configuration 2
+  # Handle file uploads for Configuration 2
   observeEvent(input$upload_button_2, {
-    if (is.null(input$SNPdb_2) && input$MUTdb_url_2 == "") {
-      output$upload_status_2 <- renderUI({
-        p("Please upload a file or provide a URL for Configuration 2.")
-      })
-    } else {
-      output$upload_status_2 <- renderUI({
-        p("Files successfully uploaded for Configuration 2!")
-      })
-    }
+    unified_inputs$wt_reads <- NULL  # Not used in this configuration
+    unified_inputs$mut_reads <- NULL  # Not used in this configuration
+    unified_inputs$SNPdb <- input$SNPdb_2
+    unified_inputs$MUTdb_url <- input$MUTdb_url_2
+    
+    output$upload_status <- renderUI({
+      p("Files successfully uploaded for Configuration 2!")
+    })
   })
   
   # Handle data processing
@@ -117,13 +179,20 @@ server <- function(input, output, session) {
       p("Processing data...")
     })
     
+    # Example: Access unified inputs for processing
+    isolate({
+      print(unified_inputs$wt_reads)
+      print(unified_inputs$mut_reads)
+      print(unified_inputs$SNPdb)
+      print(unified_inputs$MUTdb_url)
+    })
+    
     # Placeholder processing
     Sys.sleep(2)  # Simulate processing
     output$process_status <- renderUI({
       p("Done!")
     })
   })
-  
 }
 
 # Run the application
