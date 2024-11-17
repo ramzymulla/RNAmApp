ade7bc2c05693efaf2e6dc24b1b1beccf9239056
ramzymulla <rza@uoregon.edu>
2024-11-16 20:21:55 -0800

    boxes!

--- RNAmApp.R
+++ RNAmApp.R
@@ -1,17 +1,36 @@
 library(shiny)
 library(shinydashboard)
 
-# Universal accept vectors
-raw_read_accepts <- c("text/.fastq", "text/.fastq.gz",
-                      "text/.fasta", "text/.fasta.gz",
-                      "text/.fa", "text/.fa.gz",
-                      "text/.fq.gz", "text/.fq",
-                      ".fq", ".fq.gz", ".fastq", ".fastq.gz",
-                      ".fasta", ".fasta.gz", "fa", "fa.gz")
+# File Accept Vectors
+accept_vectors <- list(
+  'raw_reads' = c("text/plain","text/fastq", "text/fastq.gz",
+                        "text/fasta", "text/fasta.gz",
+                        "text/fa", "text/fa.gz",
+                        "text/fq.gz", "text/fq",
+                        ".fq", ".fq.gz", ".fastq", ".fastq.gz",
+                        ".fasta", ".fasta.gz", ".fa", ".fa.gz", 
+                        "application/x-gzip","application/x-zip"),
+  'genomes' = c("text/plain","application/x-sam", 
+                "application/x-bam", ".sam", ".bam",
+                "application/x-gzip","application/x-zip"),
+  'variants' = c("text/plain","application/x-vcf", ".vcf", 
+                 "application/x-vcf.gz", ".vcf.gz",
+                 "application/x-gzip","application/x-zip"),
+  'genemodels' = c("text/plain","application/x-gzip", ".gtf", ".gtf.gz",
+                   "application/x-gzip","application/x-zip"),
+  'folder' = c('application/x-gzip','application/x-zip', 
+               'application/x-tar')
+)
 
-genome_accepts <- c("text/.sam", "text/.bam", ".sam", ".bam")
-
-variant_accepts <- c("text/.vcf", ".vcf", "text/.vcf.gz", ".vcf.gz")
+# Unified reactive values to store inputs
+unified_data <- reactiveValues(
+  wt_reads = NULL,
+  mut_reads = NULL,
+  MUTdb = NULL,
+  MUTdb_url = NULL,
+  coverage = NULL,
+  zygosity = NULL
+)
 
 # Load modules
 source("modules/home_module.R")
@@ -44,7 +63,7 @@ ui <- dashboardPage(
     "))),
     sidebarMenu(
       menuItem("Home", tabName = "home", icon = icon("home")),
-      menuItem("Step 1: Upload Files", tabName = "step_1", icon = icon("upload")),
+      menuItem("Step 1: Upload Data", tabName = "step_1", icon = icon("upload")),
       menuItem("Step 2: Map Variants", tabName = "step_2", icon = icon("cogs")),
       menuItem("Step 3: View Candidate Mutations", tabName = "step_3", icon = icon("table"))
     )
@@ -53,7 +72,7 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       homeModuleUI("home"),
-      step1ModuleUI("step1"),
+      step1ModuleUI("step1",accept_vectors),
       step2ModuleUI("step2"),
       step3ModuleUI("step3")
     )
@@ -62,9 +81,11 @@ ui <- dashboardPage(
 
 # Define server
 server <- function(input, output, session) {
+  options(shiny.maxRequestSize = 10^5 * 1024^2)
+  
   # Call modules
   callModule(homeModule, "home")
-  callModule(step1Module, "step1")
+  callModule(step1Module, "step1", unified_data)
   callModule(step2Module, "step2")
   callModule(step3Module, "step3")
 }
--- modules/step1_module.r
+++ modules/step1_module.r
@@ -1,63 +1,102 @@
-# Universal accept vectors
-raw_read_accepts <- c("text/.fastq", "text/.fastq.gz",
-                      "text/.fasta", "text/.fasta.gz",
-                      "text/.fa", "text/.fa.gz",
-                      "text/.fq.gz", "text/.fq",
-                      ".fq", ".fq.gz", ".fastq", ".fastq.gz",
-                      ".fasta", ".fasta.gz", "fa", "fa.gz")
-
-genome_accepts <- c("text/.sam", "text/.bam", ".sam", ".bam")
-
-variant_accepts <- c("text/.vcf", ".vcf", "text/.vcf.gz", ".vcf.gz")
-
-
-step1ModuleUI <- function(id) {
+step1ModuleUI <- function(id,accept_vectors) {
   ns <- NS(id)
   tabItem(
     tabName = "step_1",
-    h3("Step 1: File Upload"),
+    h3("Step 1: Upload Data"),
     p("In this step, upload your input files for the RNA mapping pipeline."),
-    
-    tabsetPanel(
-      id = ns("file_upload_tabs"),
-      tabPanel("Raw Reads",
-               h4("File Upload for Raw Reads"),
-               fileInput(ns("wt_reads_1"), "Upload wild type RNA-seq data (FASTA or FASTQ format)",
-                         multiple = FALSE, accept = raw_read_accepts),
-               fileInput(ns("mut_reads_1"), "Upload mutant RNA-seq data (FASTA or FASTQ format)",
-                         multiple = FALSE, accept = raw_read_accepts),
-               fileInput(ns("SNPdb_1"), "Upload VCF containing known mutations",
-                         multiple = FALSE, accept = variant_accepts),
-               textInput(ns("MUTdb_url_1"), "...or provide a URL download link"),
-               actionButton(ns("upload_button_1"), "Upload Files for Configuration 1"),
-               uiOutput(ns("upload_status"))
+    fluidRow(
+      box(
+        tabsetPanel(
+          id = ns("file_upload_tabs"),
+          tabPanel("Aligned Reads",
+                   h4("File Upload for Aligned Reads"),
+                   h5("Use this tab if your RNA-seq data has already been aligned (must be aligned using HISAT2!!)."),
+                   fileInput(ns("wt_reads_aligned"), "Upload wild type RNA-seq data (SAM or BAM format)",
+                             multiple = FALSE, accept = accept_vectors$genomes),
+                   fileInput(ns("mut_reads_aligned"), "Upload mutant RNA-seq data (SAM or BAM format)",
+                             multiple = FALSE, accept = accept_vectors$genomes),
+                   fileInput(ns("MUTdb_aligned"), "Upload list of known wild-type mutations (VCF)",
+                             multiple = FALSE, accept = accept_vectors$variants),
+                   textInput(ns("MUTdb_url_aligned"), "...or provide a URL download link"),
+                   actionButton(ns("upload_button_aligned"), "Upload Files"),
+                   uiOutput(ns("upload_status"))
+            ),
+          tabPanel("Raw Reads",
+                   h4("File Upload for Raw Reads"),
+                   h5("Use this tab if your RNA-seq data has not yet been aligned to the species genome."),
+                   fileInput(ns("wt_reads_raw"), "Upload wild type RNA-seq data (FASTA or FASTQ format)",
+                             multiple = FALSE, accept = accept_vectors$raw_reads),
+                   fileInput(ns("mut_reads_raw"), "Upload mutant RNA-seq data (FASTA or FASTQ format)",
+                             multiple = FALSE, accept = accept_vectors$genomes),
+                   fileInput(ns("reference_raw"), "Upload your reference genome (FASTA or FASTQ format)",
+                             multiple = FALSE, accept = accept_vectors$raw_reads),
+                   fileInput(ns("genemodels_raw"), "Upload your gene models (GTF)",
+                             multiple = FALSE, accept = accept_vectors$genemodels),
+                   fileInput(ns("MUTdb_raw"), "Upload list of known wild-type mutations (VCF)",
+                             multiple = FALSE, accept = accept_vectors$variants),
+                   textInput(ns("MUTdb_url_raw"), "...or provide a URL download link"),
+                   actionButton(ns("upload_button_raw"), "Upload Files"),
+                   uiOutput(ns("upload_status"))
+            )
+          )
+        ),
+      box(
+        p("Please indicate the zygosity and coverage of your data."),
+        textInput(ns("zygosity"), "Zygosity"),
+        textInput(ns("coverage"), "Coverage"),
+        actionButton(ns("config_button"), "Configure Pipeline"),
+        uiOutput(ns("config_status")),
+        tags$br(),
+        actionButton(ns("run_mapper"),"Run Pipeline"),
+        uiOutput(ns("run_status"))
       ),
-      tabPanel("Aligned Reads",
-               h4("File Upload for Aligned Reads"),
-               fileInput(ns("wt_reads_2"), "Upload wild type RNA-seq data (SAM or BAM format)",
-                         multiple = FALSE, accept = genome_accepts),
-               fileInput(ns("mut_reads_2"), "Upload mutant RNA-seq data (SAM or BAM format)",
-                         multiple = FALSE, accept = genome_accepts),
-               fileInput(ns("SNPdb_2"), "Upload VCF containing known mutations",
-                         multiple = FALSE, accept = variant_accepts),
-               textInput(ns("MUTdb_url_2"), "...or provide a URL download link"),
-               actionButton(ns("upload_button_2"), "Upload Files for Configuration 2"),
-               uiOutput(ns("upload_status"))
-      )
     )
   )
 }
 
-step1Module <- function(input, output, session) {
-  observeEvent(input$upload_button_1, {
-    output$upload_status <- renderUI({
-      p("Files successfully uploaded for Configuration 1!")
-    })
-  })
+step1Module <- function(input, output, session, unified_data) {
   
-  observeEvent(input$upload_button_2, {
-    output$upload_status <- renderUI({
-      p("Files successfully uploaded for Configuration 2!")
+  observeEvent(input$config_button, {
+    unified_data$zygosity <- input$zygosity
+    unified_data$coverage <- input$coverage
+    output$config_status <- renderUI({
+      p(paste(unified_data$zygosity,' ',unified_data$coverage))
     })
   })
+
+  # Handle file uploads for raw reads
+  observeEvent(input$upload_button_1, {
+    unified_data$wt_reads <- input$wt_reads_1
+    unified_data$mut_reads <- input$mut_reads_1
+    unified_data$MUTdb <- input$MUTdb_1
+    unified_data$MUTdb_url <- input$MUTdb_url_1
+
+    output$upload_status <- renderUI({
+      p("Pipeline has been successfully initialized!")
+    })
+  })
+
+  # Handle file uploads for aligned data
+  observeEvent(input$upload_button_2, {
+    unified_data$wt_reads <- input$wt_reads_2
+    unified_data$mut_reads <- input$mut_reads_2
+    unified_data$MUTdb <- input$MUTdb_2
+    unified_data$MUTdb_url <- input$MUTdb_url_2
+
+    output$upload_status <- renderUI({
+      p("Pipeline has been successfully initialized!")
+    })
+  })
+
+  # observeEvent(input$upload_button_1, {
+  #   output$upload_status <- renderUI({
+  #     p("Files successfully uploaded for Configuration 1!")
+  #   })
+  # })
+  # 
+  # observeEvent(input$upload_button_2, {
+  #   output$upload_status <- renderUI({
+  #     p("Files successfully uploaded for Configuration 2!")
+  #   })
+  # })
 }
